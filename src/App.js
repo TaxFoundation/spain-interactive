c67b9539c78da86d7f7199aec017aa7174c1c4b1
Tom VanAntwerp <tom@tomvanantwerp.com>
2019-10-08 12:30:50 -0400

    Add initial map and render it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Spain from './map';
+import data from './data.json';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Spain {...data} />
     </div>
   );
 }
